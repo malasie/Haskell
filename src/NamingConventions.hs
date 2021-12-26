70521e594058f1c24dc457a4d9a42be3655c7a62
Maria Malasiewicz <malasie@users.noreply.github.com>
2021-12-26 17:59:38 +0100

    1

--- src/NamingConventions.hs
+++ src/NamingConventions.hs
@@ -1,15 +1,12 @@
 module Main(main) where
-import System.Win32 (xBUTTON1, COORD (yPos))
-
 data MyType=MyDataCOnstructor String 
-
 class MyClass a where
     name:: a -> String
 
 instance MyClass MyType where
     name (MyDataCOnstructor name) =name
 
-func x = xBUTTON1
+func x = x
 func' y=y
 
 main = do
